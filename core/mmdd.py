- added config option for mmd architecture - corrected eyepacs normalisation values - evolved config values - changed model structure, ep, sigma etc are now in lightning module - add eyepacs task eval metrics (separate eyepacs model class that inherits from task model) - added eyepacs mount to slurm_runner - changed slurm default config - blacked mmd code

core/mmdd.py
@@ -33,11 +33,11 @@ def assemble_loss(loss_type, loss_lambda):
     return loss_fn
 
 
-def mmd_test(x_p, x_q, model, num_permutations, alpha=0.05):
+def mmd_test(x_p, x_q, model, ep, sigma, sigma0_u, num_permutations, alpha=0.05):
 
-    ep = model.ep
-    sigma = model.sigma_sq
-    sigma0_u = model.sigma0_sq
+    # ep = model.ep
+    # sigma = model.sigma_sq
+    # sigma0_u = model.sigma0_sq
 
     S = torch.cat([x_p, x_q], 0)
     batch_size = x_p.shape[0]
@@ -340,6 +340,9 @@ class Trainer:
                 x_p,
                 x_q,
                 self.model,
+                ep,
+                sigma,
+                sigma0_u,
                 num_permutations=num_permutations,
                 alpha=self.eval_config["alpha"],
             )
@@ -349,6 +352,9 @@ class Trainer:
                 x_p,
                 x_p2,
                 self.model,
+                ep,
+                sigma,
+                sigma0_u,
                 num_permutations=num_permutations,
                 alpha=self.eval_config["alpha"],
             )

core/model.py
@@ -129,7 +129,9 @@ class BaseClassifier(pl.LightningModule):
             logger=True,
         )
 
-        return {"loss": loss}
+        y_pred = torch.argmax(y_pred, dim=1)
+
+        return {"loss": loss, "y": y, "y_pred": y_pred}
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
@@ -140,18 +142,40 @@ class BaseClassifier(pl.LightningModule):
         self.log("val/loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("val/acc", self.val_acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
-        return {"loss": loss}
+        y_pred = torch.argmax(y_pred, dim=1)
+
+        return {"loss": loss, "y": y, "y_pred": y_pred}
 
 
 class MaxKernel(BaseClassifier):
-    def __init__(self, in_channels, img_size, optim_config, loss_type, loss_lambda=10**-6):
+    def __init__(
+        self,
+        in_channels,
+        img_size,
+        optim_config,
+        loss_type,
+        loss_lambda=10**-6,
+        feature_extractor="liu",
+    ):
         super(BaseClassifier, self).__init__()
 
         self.loss_fn = assemble_loss(loss_type=loss_type, loss_lambda=loss_lambda)
 
-        model = Featurizer(n_channels=in_channels, img_size=img_size)
+        if feature_extractor == "liu":
+            self.model = Featurizer(n_channels=in_channels, img_size=img_size)
+        elif feature_extractor == "resnet50":
+            self.model = torchvision.models.resnet50(pretrained=True)
+            self.model.conv1 = nn.Conv2d(
+                in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False
+            )
+            self.model.fc = nn.Linear(self.model.fc.in_features, 128)
+        else:
+            raise ValueError(f"Feature extractor not configured: {feature_extractor}")
 
-        self.model = model
+        # Other parameters
+        self.epsilonOPT = nn.Parameter(torch.log(torch.rand(1) * 10 ** (-10)))
+        self.sigmaOPT = nn.Parameter(torch.ones(1) * np.sqrt(2 * 32 * 32))
+        self.sigma0OPT = nn.Parameter(torch.ones(1) * np.sqrt(0.005))
 
         self.optim_params = optim_config
 
@@ -167,9 +191,9 @@ class MaxKernel(BaseClassifier):
         feat_p = self.model(x_p)
         feat_q = self.model(x_q)
 
-        ep = self.model.ep
-        sigma = self.model.sigma_sq
-        sigma0_u = self.model.sigma0_sq
+        ep = self.ep
+        sigma = self.sigma_sq
+        sigma0_u = self.sigma0_sq
 
         loss = self.loss_fn(feat_p, feat_q, x_p, x_q, sigma, sigma0_u, ep)
 
@@ -185,9 +209,9 @@ class MaxKernel(BaseClassifier):
         feat_p = self.model(x_p)
         feat_q = self.model(x_q)
 
-        ep = self.model.ep
-        sigma = self.model.sigma_sq
-        sigma0_u = self.model.sigma0_sq
+        ep = self.ep
+        sigma = self.sigma_sq
+        sigma0_u = self.sigma0_sq
 
         loss = self.loss_fn(feat_p, feat_q, x_p, x_q, sigma, sigma0_u, ep)
 
@@ -200,6 +224,9 @@ class MaxKernel(BaseClassifier):
             x_p,
             x_q,
             self.model,
+            ep,
+            sigma,
+            sigma0_u,
             num_permutations=num_permutations,
             alpha=alpha,
         )
@@ -227,6 +254,18 @@ class MaxKernel(BaseClassifier):
             power = np.mean(hypothesis_rejected)
 
         self.log("val/power", power, on_epoch=True, prog_bar=True, logger=True)
+
+    @property
+    def ep(self):
+        return torch.exp(self.epsilonOPT) / (1 + torch.exp(self.epsilonOPT))
+
+    @property
+    def sigma_sq(self):
+        return self.sigmaOPT**2
+
+    @property
+    def sigma0_sq(self):
+        return self.sigma0OPT**2
 
 
 def repeated_test(x_pop, y_pop, test_fun, num_reps=100, alpha=0.05, sample_size=100):
@@ -288,14 +327,124 @@ class TaskClassifier(BaseClassifier):
         y_p_sm = torch.cat([x["y_p_sm"] for x in outputs]).detach().cpu().numpy()
         y_q_sm = torch.cat([x["y_q_sm"] for x in outputs]).detach().cpu().numpy()
 
+        batch_size = outputs[0]["y_p_sm"].shape[0]
         # TODO: check if correct: draw multi-D sample (multiple classes)
 
+        # TODO: test loop with
+        # - various sample sizes and
+        # - num_reps ~= 100
+        # - enough permutations
+
         power, type_1_err = repeated_test(
-            y_p_sm, y_q_sm, mass_ks_test, num_reps=10, alpha=0.05, sample_size=100
+            y_p_sm, y_q_sm, mass_ks_test, num_reps=100, alpha=0.05, sample_size=batch_size
         )
 
         self.log("val/power", power, on_epoch=True, prog_bar=True, logger=True)
         self.log("val/type_1err", type_1_err, on_epoch=True, prog_bar=True, logger=True)
+
+
+class EyepacsClassifier(TaskClassifier):
+    def training_epoch_end(self, outputs):
+
+        super().training_epoch_end(outputs)
+
+        # TODO (for train and val end epoch): acc, bal_acc, kappa
+        # TODO: for eyepacs only: binary onset acc
+
+        from sklearn.metrics import (
+            accuracy_score,
+            balanced_accuracy_score,
+            confusion_matrix,
+        )
+        from utils.helpers import quadratic_weighted_kappa
+
+        y = torch.cat([x["y"] for x in outputs]).detach().cpu().numpy()
+        y_pred = torch.cat([x["y_pred"] for x in outputs]).detach().cpu().numpy()
+
+        conf_mat = confusion_matrix(y, y_pred, labels=range(5))
+
+        self.log(
+            "train/bal_acc",
+            balanced_accuracy_score(y, y_pred),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        self.log(
+            "train/bin_acc_onset_1",
+            accuracy_score(y >= 1.0, y_pred >= 1.0),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        self.log(
+            "train/bin_acc_onset_2",
+            accuracy_score(y >= 2.0, y_pred >= 2.0),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        self.log(
+            "train/kappa",
+            quadratic_weighted_kappa(conf_mat),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+    def validation_epoch_end(self, outputs):
+
+        super().validation_epoch_end(outputs)
+
+        # TODO (for train and val end epoch): acc, bal_acc, kappa
+        # TODO: for eyepacs only: binary onset acc
+
+        from sklearn.metrics import (
+            accuracy_score,
+            balanced_accuracy_score,
+            confusion_matrix,
+        )
+        from utils.helpers import quadratic_weighted_kappa
+
+        y = torch.cat([x["y"] for x in outputs]).detach().cpu().numpy()
+        y_pred = torch.cat([x["y_pred"] for x in outputs]).detach().cpu().numpy()
+
+        conf_mat = confusion_matrix(y, y_pred, labels=range(5))
+
+        self.log(
+            "val/bal_acc",
+            balanced_accuracy_score(y, y_pred),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        self.log(
+            "val/bin_acc_onset_1",
+            accuracy_score(y >= 1.0, y_pred >= 1.0),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        self.log(
+            "val/bin_acc_onset_2",
+            accuracy_score(y >= 2.0, y_pred >= 2.0),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        self.log(
+            "val/kappa",
+            quadratic_weighted_kappa(conf_mat),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
 
 
 class DomainClassifier(BaseClassifier):
@@ -356,8 +505,15 @@ class DomainClassifier(BaseClassifier):
             res = permutation_test((x, y), stat_C2ST)
             return res.pvalue
 
+        batch_size = outputs[0]["y"].shape[0]
+
         power, type_1_err = repeated_test(
-            sample_p, sample_q, permutation_test_wrapper, num_reps=10, alpha=0.05, sample_size=100
+            sample_p,
+            sample_q,
+            permutation_test_wrapper,
+            num_reps=100,
+            alpha=0.05,
+            sample_size=batch_size,
         )
 
         self.log("val/power", power, on_epoch=True, prog_bar=True, logger=True)
@@ -365,9 +521,9 @@ class DomainClassifier(BaseClassifier):
 
 
 # Define the deep network for MMD-D
-class Featurizer(nn.Module):
+class FeaturizerLiu(nn.Module):
     def __init__(self, n_channels=1, img_size=32):
-        super(Featurizer, self).__init__()
+        super(FeaturizerLiu, self).__init__()
 
         def discriminator_block(in_filters, out_filters, bn=False):
             block = [
@@ -390,17 +546,28 @@ class Featurizer(nn.Module):
         ds_size = img_size // 2**4
         self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size**2, 128))
 
-        # Other parameters
-        self.epsilonOPT = nn.Parameter(torch.log(torch.rand(1) * 10 ** (-10)))
-        self.sigmaOPT = nn.Parameter(torch.ones(1) * np.sqrt(2 * 32 * 32))
-        self.sigma0OPT = nn.Parameter(torch.ones(1) * np.sqrt(0.005))
-
     def forward(self, img):
         out = self.model(img)
         out = out.view(out.shape[0], -1)
         feature = self.adv_layer(out)
 
         return feature
+
+
+class Featurizer(FeaturizerLiu):
+    """Featurizer class used for old code (including ep etc member vars)
+
+    Args:
+        FeaturizerLiu (_type_): _description_
+    """
+
+    def __init__(self, n_channels=1, img_size=32):
+        super(Featurizer, self).__init__(n_channels=n_channels, img_size=img_size)
+
+        # Other parameters
+        self.epsilonOPT = nn.Parameter(torch.log(torch.rand(1) * 10 ** (-10)))
+        self.sigmaOPT = nn.Parameter(torch.ones(1) * np.sqrt(2 * 32 * 32))
+        self.sigma0OPT = nn.Parameter(torch.ones(1) * np.sqrt(0.005))
 
     @property
     def ep(self):

utils/mmd.py
@@ -7,13 +7,13 @@ Copyright (c) 2022 Lisa Koch
 """
 
 import torch
-   
+
 
 def _mmd2_and_variance_sutherland(K_XX, K_XY, K_YY, biased=False):
     """Implementaiton of variance estimator as in Sutherland 2017 ICRL
-    
+
     Original code from https://github.com/djsutherland/opt-mmd/blob/master/two_sample/mmd.py,
-    translated from theano to PyTorch, and removed diagonal option 
+    translated from theano to PyTorch, and removed diagonal option
 
     """
 
@@ -28,8 +28,8 @@ def _mmd2_and_variance_sutherland(K_XX, K_XY, K_YY, biased=False):
     sum_diag_X = diag_X.sum()
     sum_diag_Y = diag_Y.sum()
 
-    sum_diag2_X = torch.dot(diag_X,diag_X)
-    sum_diag2_Y = torch.dot(diag_Y,diag_Y)
+    sum_diag2_X = torch.dot(diag_X, diag_X)
+    sum_diag2_Y = torch.dot(diag_Y, diag_Y)
 
     Kt_XX_sums = K_XX.sum(axis=1) - diag_X
     Kt_YY_sums = K_YY.sum(axis=1) - diag_Y
@@ -42,80 +42,92 @@ def _mmd2_and_variance_sutherland(K_XX, K_XY, K_YY, biased=False):
 
     # TODO: turn these into dot products?
     # should figure out if that's faster or not on GPU / with theano...
-    Kt_XX_2_sum = (K_XX ** 2).sum() - sum_diag2_X
-    Kt_YY_2_sum = (K_YY ** 2).sum() - sum_diag2_Y
-    K_XY_2_sum  = (K_XY ** 2).sum()
+    Kt_XX_2_sum = (K_XX**2).sum() - sum_diag2_X
+    Kt_YY_2_sum = (K_YY**2).sum() - sum_diag2_Y
+    K_XY_2_sum = (K_XY**2).sum()
 
     if biased:
-        mmd2 = ((Kt_XX_sum + sum_diag_X) / (m * m)
-              + (Kt_YY_sum + sum_diag_Y) / (m * m)
-              - 2 * K_XY_sum / (m * m))
+        mmd2 = (
+            (Kt_XX_sum + sum_diag_X) / (m * m)
+            + (Kt_YY_sum + sum_diag_Y) / (m * m)
+            - 2 * K_XY_sum / (m * m)
+        )
     else:
-        mmd2 = (Kt_XX_sum / (m * (m-1))
-              + Kt_YY_sum / (m * (m-1))
-              - 2 * K_XY_sum / (m * m))
+        mmd2 = Kt_XX_sum / (m * (m - 1)) + Kt_YY_sum / (m * (m - 1)) - 2 * K_XY_sum / (m * m)
 
-    torch.dot(Kt_YY_sums,K_XY_sums_0)
+    torch.dot(Kt_YY_sums, K_XY_sums_0)
 
     var_est = (
-          2 / (m**2 * (m-1)**2) * (
-              2 * torch.dot(Kt_XX_sums,Kt_XX_sums) - Kt_XX_2_sum
-            + 2 * torch.dot(Kt_YY_sums,Kt_YY_sums) - Kt_YY_2_sum)
-        - (4*m-6) / (m**3 * (m-1)**3) * (Kt_XX_sum**2 + Kt_YY_sum**2)
-        + 4*(m-2) / (m**3 * (m-1)**2) * (
-              torch.dot(K_XY_sums_1,K_XY_sums_1)
-            + torch.dot(K_XY_sums_0,K_XY_sums_0))
-        - 4 * (m-3) / (m**3 * (m-1)**2) * K_XY_2_sum
-        - (8*m - 12) / (m**5 * (m-1)) * K_XY_sum**2
-        + 8 / (m**3 * (m-1)) * (
-              1/m * (Kt_XX_sum + Kt_YY_sum) * K_XY_sum
-            - torch.dot(Kt_XX_sums,K_XY_sums_1)
-            - torch.dot(Kt_YY_sums,K_XY_sums_0))
+        2
+        / (m**2 * (m - 1) ** 2)
+        * (
+            2 * torch.dot(Kt_XX_sums, Kt_XX_sums)
+            - Kt_XX_2_sum
+            + 2 * torch.dot(Kt_YY_sums, Kt_YY_sums)
+            - Kt_YY_2_sum
+        )
+        - (4 * m - 6) / (m**3 * (m - 1) ** 3) * (Kt_XX_sum**2 + Kt_YY_sum**2)
+        + 4
+        * (m - 2)
+        / (m**3 * (m - 1) ** 2)
+        * (torch.dot(K_XY_sums_1, K_XY_sums_1) + torch.dot(K_XY_sums_0, K_XY_sums_0))
+        - 4 * (m - 3) / (m**3 * (m - 1) ** 2) * K_XY_2_sum
+        - (8 * m - 12) / (m**5 * (m - 1)) * K_XY_sum**2
+        + 8
+        / (m**3 * (m - 1))
+        * (
+            1 / m * (Kt_XX_sum + Kt_YY_sum) * K_XY_sum
+            - torch.dot(Kt_XX_sums, K_XY_sums_1)
+            - torch.dot(Kt_YY_sums, K_XY_sums_0)
+        )
     )
 
     return mmd2, var_est
 
-def _mmd2_and_variance(K_XX, K_XY, K_YY, biased=False):
-    """Implementation of variance estimator as in Liu 2020 ICML
 
-    """
+def _mmd2_and_variance(K_XX, K_XY, K_YY, biased=False):
+    """Implementation of variance estimator as in Liu 2020 ICML"""
     m = K_XX.shape[0]  # Assumes X, Y are same shape
 
-    hh = K_XX + K_YY - K_XY - K_XY.transpose(0,1)   
+    hh = K_XX + K_YY - K_XY - K_XY.transpose(0, 1)
     hh_diag = torch.diagonal(hh)
-    
-    if biased:
-        mmd2 = hh.sum() / (m*m)
-    else:   
-        mmd2 = (hh.sum() - hh_diag.sum()) / (m * (m-1))
 
-    V1 = torch.dot(hh.sum(1)/m,hh.sum(1)/m) / m
+    if biased:
+        mmd2 = hh.sum() / (m * m)
+    else:
+        mmd2 = (hh.sum() - hh_diag.sum()) / (m * (m - 1))
+
+    V1 = torch.dot(hh.sum(1) / m, hh.sum(1) / m) / m
     V2 = hh.sum() / m**2
-    var_est = 4*(V1 - V2**2)
+    var_est = 4 * (V1 - V2**2)
 
     return mmd2, var_est, None
 
-def mmd_and_var(f_x, f_y, x, y, f_sigma, sigma, epsilon=10**-10, biased=False,
-                sutherland=False):
-    
+
+def mmd_and_var(f_x, f_y, x, y, f_sigma, sigma, epsilon=10**-10, biased=False, sutherland=False):
+
     K_xx = kernel_liu(f_x, f_x, x, x, f_sigma, sigma, epsilon)
     K_yy = kernel_liu(f_y, f_y, y, y, f_sigma, sigma, epsilon)
     K_xy = kernel_liu(f_x, f_y, x, y, f_sigma, sigma, epsilon)
-    
+
     if sutherland:
         return _mmd2_and_variance_sutherland(K_xx, K_xy, K_yy, biased=biased)
     else:
         return _mmd2_and_variance(K_xx, K_xy, K_yy, biased=biased)
 
+
 def kernel_liu(f_x, f_y, x, y, f_sigma, sigma, epsilon=10**-10):
-    kernel = (1 - epsilon) * gaussian_kernel(f_x, f_y, f_sigma) * gaussian_kernel(x, y, sigma) + epsilon * gaussian_kernel(x, y, sigma)
+    kernel = (1 - epsilon) * gaussian_kernel(f_x, f_y, f_sigma) * gaussian_kernel(
+        x, y, sigma
+    ) + epsilon * gaussian_kernel(x, y, sigma)
     return kernel
-    
+
+
 def gaussian_kernel(x, y, sigma):
-    """Gaussian kernel. Note that sigma = (2*sig**2)
-    """
-    ret_val = torch.exp( - pdist(x, y)**2 / sigma)
+    """Gaussian kernel. Note that sigma = (2*sig**2)"""
+    ret_val = torch.exp(-pdist(x, y) ** 2 / sigma)
     return ret_val
+
 
 def pdist(x, y):
     """calculate pairwise distances on tensors without batch dimension
@@ -127,17 +139,17 @@ def pdist(x, y):
     Returns:
         [type]: tensor of shape [n, m]. normed distance!
     """
-    
+
     # create batch dimension to use torch batched pairwise distance calculation
     b_x = x.view(1, *x.shape)
     b_y = y.view(1, *y.shape)
     b_dist = torch.cdist(b_x, b_y, p=2)
-    
+
     # remove batch dimension
     dist = b_dist.view(*b_dist.shape[1:])
-    
+
     return dist
-    
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     pass